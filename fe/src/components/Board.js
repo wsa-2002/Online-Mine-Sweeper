b82f563f4773c460def3ff2834f9cad2283005b6
amber14liu <yachiehliu0414@gmail.com>
2022-12-03 14:29:09 +0800

    fix web socket

--- fe/src/components/Board.js
+++ fe/src/components/Board.js
@@ -13,7 +13,7 @@ import Dashboard from "./Dashboard";
 import createBoard from "../util/createBoard";
 import { revealed } from "../util/reveal";
 import "./css/Board.css";
-import { useReady } from "../hooks/websocket";
+import { sendReady, useReceiveMsg } from "../hooks/websocket";
 import CountDown from "./CountDown";
 
 const Board = ({ boardSize, mineNum, backToHome }) => {
@@ -23,13 +23,12 @@ const Board = ({ boardSize, mineNum, backToHome }) => {
 	const [gameOver, setGameOver] = useState(false); // A boolean variable. If true, means you lose the game (Game over).
 	const [remainFlagNum, setRemainFlagNum] = useState(0); // An integer variable to store the number of remain flags.
 	const [win, setWin] = useState(false); // A boolean variable. If true, means that you win the game.
-
-	const { sendReady, readyRes } = useReady(); // web socket
 	const [myTurn, setMyTurn] = useState(false); // 1 -> this player's turn
 	const [countDown, setCountDown] = useState(false); // 1 -> show countdown page
 	const [gameStart, setGameStart] = useState(false); // 1 -> game start
 	const [ready, setReady] = useState(false); // 1 -> this player is ready
 	const [startTime, setStartTime] = useState(null);
+	const { task, data } = useReceiveMsg("ready");
 	const username = "hello";
 	const nowww = new Date(); // debug
 
@@ -38,25 +37,24 @@ const Board = ({ boardSize, mineNum, backToHome }) => {
 	}, []);
 
 	useEffect(() => {
-		if (readyRes && ready && !countDown && !gameStart) {
-			//setStartTime(new Date(readyRes.start_time));
+		if (data) {
+			//setStartTime(new Date(data.start_time));
 			setStartTime(new Date(nowww.getTime() + 6000));
 			let diff = 0;
 			const timeIntervalId = setInterval(() => {
 				const now = new Date();
 				diff = timeDiff(now, new Date(nowww.getTime() + 6000));
-				//diff = timeDiff(now, new Date(readyRes.start_time));
+				//diff = timeDiff(now, new Date(data.start_time));
 				if (diff <= 3) {
 					clearInterval(timeIntervalId);
-					// console.log("time diff less than 3 =", diff, ", count down start");
 					setCountDown(true);
-					if (readyRes.turns === username) {
-						setMyTurn(true);
-					}
+					// if (data.turns === username) {
+					// 	setMyTurn(true);
+					// }
 				}
 			}, 1000);
 		}
-	}, [readyRes, ready]);
+	}, [data]);
 
 	const timeDiff = (now, start) => {
 		const gap = start - now;
--- fe/src/hooks/websocket.js
+++ fe/src/hooks/websocket.js
@@ -1,11 +1,17 @@
 import { useEffect, useState } from "react";
-// client side
+
 const client = new WebSocket("ws://localhost:8000/socket");
 
-const username = "whoami"; // string
+client.onopen = () => {
+	console.log("connection established");
+};
+
+client.onclose = () => {
+	console.log("connection closed");
+};
 
 const sendData = async (data) => {
-	await client.send(JSON.stringify(data));
+	client.send(JSON.stringify(data));
 };
 
 const sendSetup = (data) => {
@@ -13,90 +19,35 @@ const sendSetup = (data) => {
 };
 
 const sendReady = (data) => {
-	sendData({ task: "ready", username: username, data: data });
+	sendData({ task: "ready", ...data });
 };
 
 const sendAction = (data) => {
-	sendData({ task: "action", username: username, data: data });
+	sendData({ task: "action", ...data });
 };
 
 const sendCheckStatus = (data) => {
-	sendData({ task: "check_status", username: username, data: data });
+	sendData({ task: "check_status", ...data });
 };
 
-client.onopen = () => {
-	console.log("hello");
-};
-
-client.onclose = () => {
-	console.log("connection closed");
-};
-
-// home page
-const useSetup = () => {
-	const [setupRes, setSetupRes] = useState(null);
+const useReceiveMsg = (taskLabel) => {
+	const [data, setData] = useState(null);
+	const [task, setTask] = useState(null);
 
 	client.onmessage = (bytestring) => {
 		var { data } = bytestring;
 		var { task, data, error } = JSON.parse(data);
-
-		if (task === "setup") {
-			setSetupRes(data);
-		}
-	};
-	return { sendSetup, setupRes };
-};
-
-// board page
-const useReady = () => {
-	const [readyRes, setReadyRes] = useState(null);
-
-	client.onmessage = (bytestring) => {
-		var { data } = bytestring;
-		var { task, data, error } = JSON.parse(data);
-
-		if (task === "ready") {
-			setReadyRes(data);
+		if (task === taskLabel) {
+			if (error) {
+				console.log("server error msg:", error);
+				return;
+			}
+			setTask(task);
+			setData(data);
 		}
 	};
 
-	// 模擬input可以先這樣寫
-	useEffect(() => {
-		setReadyRes({
-			start_time: "2022-11-29T21:25:00",
-			turns: "string", // username
-		});
-	}, []);
-
-	return { sendReady, readyRes };
+	return { task, data };
 };
 
-// board page
-const useUpdate = () => {
-	const [updateRes, setUpdateRes] = useState(null);
-
-	client.onmessage = (bytestring) => {
-		var { data } = bytestring;
-		var { task, data, error } = JSON.parse(data);
-		if (task === "update_board") {
-			setUpdateRes(data);
-		}
-	};
-	return { sendAction, updateRes };
-};
-
-// board page
-const useCheck = () => {
-	const [checkRes, setCheckRes] = useState(null);
-
-	client.onmessage = (bytestring) => {
-		var { data } = bytestring;
-		var { task, data, error } = JSON.parse(data);
-		if (task === "check_status") {
-			setCheckRes(data);
-		}
-	};
-	return { sendCheckStatus, checkRes };
-};
-
-export { useReady, useCheck, useSetup, useUpdate };
+export { useReceiveMsg, sendSetup, sendReady, sendAction, sendCheckStatus };
