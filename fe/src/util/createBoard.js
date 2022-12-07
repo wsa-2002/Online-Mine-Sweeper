4419ec8916ebb9ac7fcdd11054edfaa9d1b062a2
amber14liu <yachiehliu0414@gmail.com>
2022-12-07 16:03:26 +0800

    change board appearance

--- fe/src/util/createBoard.js
+++ fe/src/util/createBoard.js
@@ -6,66 +6,24 @@
   Copyright     [ 2021 10 ]
 ****************************************************************************/
 
-import randomNum from "./randomFixSeed";
+// import randomNum from "./randomFixSeed";
 
 // eslint-disable-next-line
-export default (boardSize, mineNum) => {
-  let board = [];
-  let mineLocations = [];
+export default (boardSize) => {
+	let board = [];
 
-  // // Print Board function (For testing)
-  // const printBoard = () => {
-  //   console.log("Current Board");
-  //   for (let x = 0; x < boardSize; x++) {
-  //     console.log(
-  //       board[x].map((x) => {
-  //         return x.value !== "💣" ? x.value.toString() + " " : x.value;
-  //       })
-  //     );
-  //   }
-  // };
+	// Create a blank board
+	for (let x = 0; x < boardSize; x++) {
+		let subCol = [];
+		for (let y = 0; y < boardSize; y++) {
+			subCol.push({
+				value: -3,
+				x: x,
+				y: y,
+			});
+		}
+		board.push(subCol);
+	}
 
-  // Create a blank board
-  for (let x = 0; x < boardSize; x++) {
-    let subCol = [];
-    for (let y = 0; y < boardSize; y++) {
-      subCol.push({
-        value: 0, // To store the number of mines around the cell.
-        revealed: false, // To store if the cell is revealed.
-        x: x, // To store the x coordinate (the column index) of the cell.
-        y: y, // To store the y coordinate (the row index) of the cell.
-        flagged: false, // To store if the cell is flagged.
-      });
-    }
-    board.push(subCol);
-  }
-
-  // Random bombs locations
-  let mineCount = 0;
-  while (mineCount < mineNum) {
-    let x = randomNum(0, boardSize - 1);
-    let y = randomNum(0, boardSize - 1);
-
-    if (board[x][y].value === 0) {
-      // Check this location has not been located a mine.
-      board[x][y].value = "💣"; // Change the value of the cell to '💣'
-      mineLocations.push([x, y]);
-      mineCount++;
-    }
-  }
-
-  mineLocations.map((mine) => {
-    for (let x = mine[0] - 1; x <= mine[0] + 1; x++) {
-      for (let y = mine[1] - 1; y <= mine[1] + 1; y++) {
-        if (x >= 0 && x < boardSize && y >= 0 && y < boardSize) {
-          if (board[x][y].value !== "💣") {
-            board[x][y].value++;
-          }
-        }
-      }
-    }
-    return mine;
-  });
-
-  return { board, mineLocations };
+	return board;
 };
